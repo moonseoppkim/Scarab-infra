3abd518012ce41ba7815dc92fc1de4042e4b5c73
Ubuntu <seop@seop.rdxiiqek1d3ufm1or4d4aue32e.rx.internal.cloudapp.net>
2024-10-26 09:28:24 +0000

    1026 compulsory miss check with base address

--- seop/cse220_home/scarab/src/libs/cache_lib.c
+++ seop/cse220_home/scarab/src/libs/cache_lib.c
@@ -277,14 +277,12 @@ void* cache_access(Cache* cache, Addr addr, Addr* line_addr, Flag update_repl) {
 
   if(strcmp(cache->name, "DCACHE") == 0 || strcmp(cache->name, "FA_DCACHE") == 0) {
     Flag new_entry = FALSE;
-    void* value = hash_table_access_create(&cache->accessed_blocks, *line_addr, &new_entry);
+    hash_table_access_create(&cache->accessed_blocks, *line_addr - (*line_addr % cache->line_size), &new_entry);
 
     if (new_entry) {
         cache->is_compulsory_miss = TRUE;
         cache->is_conflict_miss = FALSE;
         cache->is_capacity_miss = FALSE;
-        Flag *dummy = (Flag *) value;
-        *dummy = TRUE;
     } else {
       if(strcmp(cache->name, "DCACHE") == 0) {
         cache->is_compulsory_miss = FALSE;
